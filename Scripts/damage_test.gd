6cb5e36bb8fa2d5ad72ee0a1cef94156c1d87769
Alabin07 <a.folketoft@gmail.com>
2023-10-19 12:40:40 +0200

    limit po ammo

--- Scripts/damage_test.gd
+++ Scripts/damage_test.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
 @onready var projectile = preload("res://Sprites/projectile.tscn")
+var bullet = 1
+@onready var timer1 = $Timer
+var timerReady = true
 
 func _ready():
 	pass
 
 func _process(_delta):
 	var enemy_group = get_tree().get_nodes_in_group("enemy")
-	for enemy in enemy_group:
-		var distance = self.global_position.distance_to(enemy.global_position)
+	
+	if bullet > 0 and timerReady:
+		for enemy in enemy_group:
+			var distance = self.global_position.distance_to(enemy.global_position)
 
-		if distance < 200:
-			var new_projectile = projectile.instantiate()
-			add_child(new_projectile)
-			
-			new_projectile.target = enemy
+			if distance < 200:
+				var new_projectile = projectile.instantiate()
+				add_child(new_projectile)
 
+				new_projectile.target = enemy
+				bullet -= 1
+				timerReady = false
+				timer1.start()
 
-
-func _on_timer_timeout():
-	pass
+func _on_rpm_timer_timeout():
+	timerReady = true
+	bullet += 1
